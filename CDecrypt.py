302d8f660d6bda990861a16045285c06bbca3f55
Nick <nickvmorello25@gmail.com>
2021-09-30 19:25:21 -0700

    updated CDecrypt and input/output to work remotely

--- CDecrypt.py
+++ CDecrypt.py
@@ -1,33 +1,39 @@
-def encrypt(text,s):
-    result = ""# transverse the plain text
-    for i in range(len(text)):
-        char = text[i]# Encrypt uppercase characters in plain text
-        if (char.isupper()):
-            result += chr((ord(char) + s-65) % 26 + 65)# Encrypt lowercase characters in plain text
-        else:
-            result += chr((ord(char) + s - 97) % 26 + 97)
-    return result
-#check the above function
-text = "CEASER CIPHER DEMO"
-s = 7
+import argparse
+import os
+import time
+#import socket
+import sys
+import string
+#from aes import AESCipher
+#from Crypto.PublicKey import RSA
 
-print ("Plain Text : " + text)
-print ("Shift pattern : " + str(s))
-print ("Cipher: " + encrypt(text,s))
+# # Handle command-line arguments
+# parser = argparse.ArgumentParser()
+# parser.add_argument("-c", "--cipher",
+#                     help='cipher... caesar or julia',
+#                     required=True)
+# parser.add_argument("-d", "--decrypt",
+#                     help='Decrypt a file',
+#                     required=False)
+# parser.add_argument("-e", "--encrypt",
+#                     help='Encrypt a file',
+#                     required=False)
+# parser.add_argument("-o", "--outfile",
+#                     help='Output file',
+#                     required=False)
 
-
-message = 'FLYZHUVKFULAOYLLZSVHKLKWSHBKPAZKLJWYVWOLZFPUNNSHBJVTHZZALUJPSSPUNYLAVYALKAOVAOHYYVDZDOPTZLFWSHUALKWYLMPNBYLHWWSLZLLKZRHALZHJLZPTWBYPAPLZTPUPHABYPGLZJHSJBSHAVYZKPZWLYZPVUZZBWLYZAHYZMVYLZLLUDOPAASLZRULL' #encrypted message
+# args = parser.parse_args()
+MESSEGE = 'OLSSV'
+# MESSEGE = 'FLYZHUVKFULAOYLLZSVHKLKWSHBKPAZKLJWYVWOLZFPUNNSHBJVTHZZALUJPSSPUNYLAVYALKAOVAOHYYVDZDOPTZLFWSHUALKWYLMPNBYLHWWSLZLLKZRHALZHJLZPTWBYPAPLZTPUPHABYPGLZJHSJBSHAVYZKPZWLYZPVUZZBWLYZAHYZMVYLZLLUDOPAASLZRULL'
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-for key in range(len(LETTERS)):
-    translated = ''
-    for symbol in message:
-        if symbol in LETTERS:
-            num = LETTERS.find(symbol)
-            num = num - key
-            if num < 0:
-                num = num + len(LETTERS)
-            translated = translated + LETTERS[num]
+def decrypt(key, messege):
+    plaintext = ''
+    for i in range(len(messege)):
+        if ((ord(messege[i])-key >= 65 and ord(messege[i])-key <= 90)):
+            plaintext+=chr(ord(messege[i])-key)
         else:
-            translated = translated + symbol
-    print('Hacking key #%s: %s' % (key, translated))
+            plaintext += ' '
+    print(plaintext)
+    
+decrypt(7,MESSEGE)
