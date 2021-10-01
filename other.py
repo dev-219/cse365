54abe4bc61e0ba72d5c949a7d09bd1f51991e4db
Nick <nickvmorello@gmail.com>
2021-10-01 15:18:48 -0700

    laptop dying, leaving tutoring, pushing to work at desktop

--- other.py
+++ other.py
@@ -1,6 +1,9 @@
 import gzip
 import shutil
-with gzip.open('/home/student/cse365/tarball/195/withkey/juliaplaintext.txt.gz.enc', 'rb') as f_in:
-    with open('output2.txt', 'wb') as f_out:
-        shutil.copyfileobj(f_in, f_out)
+#'/home/student/cse365/tarball/195/withkey/juliaplaintext.txt.gz.enc'
+byte_ary = []
+with open('/home/student/cse365/tarball/195/withkey/juliaplaintext.txt.gz.enc', 'rb') as f:
+    byte_s = f.read(535)
+    print(byte_s)
+
         
